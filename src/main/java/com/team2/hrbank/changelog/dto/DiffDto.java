3523a17ae412ec9b2682e8fe047687d942b296fc
김재원 <dumdum7@hs.ac.kr>
2025-10-23 11:21:19 +0900

    fix: Builder 대신 static factory method로 변경

--- src/main/java/com/team2/hrbank/changelog/dto/DiffDto.java
+++ src/main/java/com/team2/hrbank/changelog/dto/DiffDto.java
@@ -2,10 +2,22 @@ package com.team2.hrbank.changelog.dto;
 
 import lombok.Builder;
 
-@Builder
 public record DiffDto(
         String propertyName,
         String before,
         String after
 ) {
+
+    public DiffDto with(String propertyName, String before, String after) {
+        return new DiffDto(propertyName, before, after);
+    }
+
+    public DiffDto withBefore(String propertyName, String before) {
+        return new DiffDto(propertyName, before, null);
+    }
+
+    public DiffDto withAfter(String propertyName, String after) {
+        return new DiffDto(propertyName, null, after);
+    }
+
 }
