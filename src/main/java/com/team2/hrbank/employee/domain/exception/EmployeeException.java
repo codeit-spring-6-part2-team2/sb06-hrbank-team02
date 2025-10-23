e165171612199ce706dbb336f558aa235c570906
Kim Seung-Been <mainlib990@gmail.com>
2025-10-23 16:01:31 +0900

    feat(employee): Employee 도메인 구현

---
+++ src/main/java/com/team2/hrbank/employee/domain/exception/EmployeeException.java
@@ -0,0 +1,8 @@
+package com.team2.hrbank.employee.domain.exception;
+
+public class EmployeeException extends RuntimeException {
+
+    public EmployeeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/team2/hrbank/employee/domain/exception/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.team2.hrbank.employee.domain.exception;
+
+import org.jspecify.annotations.NullMarked;
