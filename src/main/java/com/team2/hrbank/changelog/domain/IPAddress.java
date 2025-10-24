55d57bf74fb928a665ab49a7748729ed9dd9128f
김재원 <dumdum7@hs.ac.kr>
2025-10-24 16:30:25 +0900

    fix: IPAddress을 불변 클래스로 변경

--- src/main/java/com/team2/hrbank/changelog/domain/IPAddress.java
+++ src/main/java/com/team2/hrbank/changelog/domain/IPAddress.java
@@ -9,12 +9,10 @@ import lombok.NoArgsConstructor;
 import java.util.regex.Pattern;
 
 @Embeddable
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED, onConstructor_ = @Deprecated)
-public class IPAddress {
-
-    @Column(name = "ip_address", nullable = false)
-    private String ipAddress;
+public record IPAddress(
+        @Column(name = "ip_address", nullable = false)
+        String ipAddress
+) {
 
     private static final Pattern IPV4_PATTERN = Pattern.compile(
             "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
@@ -24,24 +22,10 @@ public class IPAddress {
             "^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$|^::1$|^::$"
     );
 
-    protected IPAddress(String ipAddress) {
-        this.ipAddress = ipAddress;
-    }
-
-    private static void isValidAddress(String ipAddress) {
-
-        if (!IPV4_PATTERN.matcher(ipAddress).matches() && !IPV6_PATTERN.matcher(ipAddress).matches()) {
+    public IPAddress {
+        if (!IPV4_PATTERN.matcher(ipAddress).matches() || !IPV6_PATTERN.matcher(ipAddress).matches()) {
             throw new IllegalArgumentException("유효하지 않은 IP 주소입니다: " + ipAddress);
         }
-
-    }
-
-    public static IPAddress of(String ipAddress) {
-
-        isValidAddress(ipAddress);
-
-        return new IPAddress(ipAddress);
-
     }
 
 }
