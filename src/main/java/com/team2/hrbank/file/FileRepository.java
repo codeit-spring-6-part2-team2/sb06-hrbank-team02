b01b64fb694c4954fe5868ca783cefc636a7ab01
SeoGyeongWon <skwknh00@naver.com>
2025-10-24 15:14:18 +0900

    FIX: build.gradle querydsl 부분 수정, File클래스명 FileEntity로 변경, FileRepository import 클래스명 변경

--- src/main/java/com/team2/hrbank/file/FileRepository.java
+++ src/main/java/com/team2/hrbank/file/FileRepository.java
@@ -2,5 +2,6 @@ package com.team2.hrbank.file;
 
 import com.team2.hrbank.file.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.team2.hrbank.file.domain.FileEntity;
 
-public interface FileRepository extends JpaRepository<File, Long> {}
+public interface FileRepository extends JpaRepository<FileEntity,Long>{ }
