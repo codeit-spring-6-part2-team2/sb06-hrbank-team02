fec25ca23d9057584901cd4a6970d4af871a1a9d
김재원 <dumdum7@hs.ac.kr>
2025-10-24 10:01:40 +0900

    refactor: refactor

--- src/main/java/com/team2/hrbank/changelog/domain/IPAddress.java
+++ src/main/java/com/team2/hrbank/changelog/domain/IPAddress.java
@@ -16,18 +16,16 @@ public class IPAddress {
     @Column(name = "ip_address", nullable = false)
     private String ipAddress;
 
-    private static final Pattern IPV4_PATTERN =
-            Pattern.compile("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
-
-    private static final Pattern IPV6_PATTERN =
-            Pattern.compile("^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$|^::1$|^::$");
-
     protected IPAddress(String ipAddress) {
         this.ipAddress = ipAddress;
     }
 
     private static boolean isValidAddress(String ipAddress) {
-        return IPV4_PATTERN.matcher(ipAddress).matches() || IPV6_PATTERN.matcher(ipAddress).matches();
+
+        String ipV4Regex = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+        String ipV6Regex = "^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$|^::1$|^::$";
+
+        return ipAddress.matches(ipV4Regex) || ipAddress.matches(ipV6Regex);
     }
 
     public static IPAddress of(String ipAddress) {
