c24e8bc27a44467f3909c78af27c3ee256933c33
김재원 <dumdum7@hs.ac.kr>
2025-10-24 16:27:58 +0900

    fix: EmployeeEmail을 불변 클래스로 변경

--- src/main/java/com/team2/hrbank/changelog/domain/EmployeeEmail.java
+++ src/main/java/com/team2/hrbank/changelog/domain/EmployeeEmail.java
@@ -7,33 +7,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED, onConstructor_ = @Deprecated)
-public class EmployeeEmail {
+public record EmployeeEmail(
+        @Column(name = "email", nullable = false)
+        String email
+) {
 
-    @Column(name = "email", nullable = false)
-    private String email;
+    private static final String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
 
-    protected EmployeeEmail(String email) {
-        this.email = email;
-    }
-
-    public static void validateEmail(String email) {
-
-        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-
-        if (email == null || !email.matches(emailRegex)) {
+    public EmployeeEmail {
+        if (!email.matches(emailRegex)) {
             throw new IllegalArgumentException("유효하지 않은 이메일 주소입니다: " + email);
         }
-
-    }
-
-    public static EmployeeEmail of(String email) {
-
-        validateEmail(email);
-
-        return new EmployeeEmail(email);
-
     }
 
 }
