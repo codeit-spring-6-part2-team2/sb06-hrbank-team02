3ea092289c54ef2bf538e89bf81a136325dbb194
SeoGyeongWon <skwknh00@naver.com>
2025-10-24 15:18:01 +0900

    FIX: FileRepository.java import 클래스명 변경 File -> FileEntity

--- src/main/java/com/team2/hrbank/file/FileRepository.java
+++ src/main/java/com/team2/hrbank/file/FileRepository.java
@@ -1,6 +1,5 @@
 package com.team2.hrbank.file;
 
-import com.team2.hrbank.file.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.team2.hrbank.file.domain.FileEntity;
 
