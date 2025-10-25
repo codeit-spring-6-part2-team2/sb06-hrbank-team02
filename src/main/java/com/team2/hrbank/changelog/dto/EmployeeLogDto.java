6a6e747255ce9a62b9f6de1deca3c7f2490cef0a
김재원 <dumdum7@hs.ac.kr>
2025-10-25 11:21:56 +0900

    fix: DTO 구조를 더 간단하게 변경

--- src/main/java/com/team2/hrbank/changelog/dto/EmployeeLogDto.java
+++ src/main/java/com/team2/hrbank/changelog/dto/EmployeeLogDto.java
@@ -11,7 +11,8 @@ public class EmployeeLogDto {
     }
 
     @Builder
-    public record EmployeeBaseInfo(
+    public record CreateLogRequest(
+            // Employee info
             LocalDateTime hireDate,
             String name,
             String position,
@@ -19,43 +20,51 @@ public class EmployeeLogDto {
             String email,
             String employeeNumber,
             String status,
-            //삭제 여부를 판단하기 위한 필드
-            boolean isDeleted
-    ) {}
-
-    public record LogMetadata(
-            @Nullable
-            String memo,
+            boolean isDeleted,
+            // metadata
+            @Nullable String memo,
             String ipAddress
-    ) {
-
-        public static LogMetadata withMemo(String ipAddress, String memo) {
-            return new LogMetadata(memo, ipAddress);
-        }
-
-        public static LogMetadata withoutMemo(String ipAddress) {
-            return new LogMetadata(null, ipAddress);
-        }
-
-    }
-
-    @Builder
-    public record CreateLogRequest(
-            EmployeeBaseInfo employeeInfo,
-            LogMetadata metadata
     ) {}
 
     @Builder
     public record CreateUpdateLogRequest(
-            EmployeeBaseInfo before,
-            EmployeeBaseInfo after,
-            LogMetadata metadata
+            // Before Employee info
+            LocalDateTime beforeHireDate,
+            String beforeName,
+            String beforePosition,
+            String beforeDepartment,
+            String beforeEmail,
+            String beforeEmployeeNumber,
+            String beforeStatus,
+            boolean beforeIsDeleted,
+            // After Employee info
+            LocalDateTime afterHireDate,
+            String afterName,
+            String afterPosition,
+            String afterDepartment,
+            String afterEmail,
+            String afterEmployeeNumber,
+            String afterStatus,
+            boolean afterIsDeleted,
+            // metadata
+            @Nullable String memo,
+            String ipAddress
     ) {}
 
     @Builder
     public record CreateDeleteLogRequest(
-            EmployeeBaseInfo employeeInfo,
-            LogMetadata metadata
+            // Employee info
+            LocalDateTime hireDate,
+            String name,
+            String position,
+            String department,
+            String email,
+            String employeeNumber,
+            String status,
+            boolean isDeleted,
+            // metadata
+            @Nullable String memo,
+            String ipAddress
     ) {}
 
 }
