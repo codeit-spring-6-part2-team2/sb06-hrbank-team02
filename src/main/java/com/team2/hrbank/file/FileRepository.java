8e59ab6012694f369aec985fcfa197070184b6ec
SeoGyeongWon <skwknh00@naver.com>
2025-10-27 18:12:32 +0900

    FIX: @Repository 어노테이션 추가

--- src/main/java/com/team2/hrbank/file/FileRepository.java
+++ src/main/java/com/team2/hrbank/file/FileRepository.java
@@ -2,5 +2,7 @@ package com.team2.hrbank.file;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.team2.hrbank.file.domain.FileEntity;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FileRepository extends JpaRepository<FileEntity,Long>{ }
