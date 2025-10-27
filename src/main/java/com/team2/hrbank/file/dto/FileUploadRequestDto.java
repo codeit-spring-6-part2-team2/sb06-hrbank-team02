bd014c0bad5ac0cfccca426e0128cab580e26dc7
SeoGyeongWon <skwknh00@naver.com>
2025-10-27 18:10:11 +0900

    FIX: DTO 백업 관리 기능 구현을 위한 uplad,download dto 구현

--- src/main/java/com/team2/hrbank/file/dto/FileUploadRequestDto.java
+++ src/main/java/com/team2/hrbank/file/dto/FileUploadRequestDto.java
@@ -1,16 +1,14 @@
 package com.team2.hrbank.file.dto;
 
 import com.team2.hrbank.file.domain.OwnerType;
-import lombok.Builder;
 
-@Builder
-public record FileDownloadResponseDto(
-    Long fileId,
+public record FileUploadRequestDto(
     String fileName,
     String fileType,
     Long fileSize,
-    String filePath,
     OwnerType ownerType,
     Long ownerId,
     byte[] fileData
-) {}
+) {
+
+}
